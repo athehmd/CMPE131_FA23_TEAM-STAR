ecb5d397bea35497bb12776e7f49ec357ea5d544
athehmd <36544534+athehmd@users.noreply.github.com>
2023-12-01 21:19:39 -0800

    Added Use Functionality

--- create_db.sql
+++ create_db.sql
@@ -1,4 +1,6 @@
 create database City_Of_Williamston;
 
+use City_of_Williamston;
+
 create table UserInfo(Username varchar(20) NOT NULL, PwHash varbinary(1024) NOT NULL NOT NULL, CDate datetime, PRIMARY KEY(Username));
 
