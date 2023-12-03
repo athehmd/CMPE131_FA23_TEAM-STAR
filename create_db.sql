5199b70d3552cbe0ce942b1b9b1efa6a9fe210fc
Mark6428 <143025211+Mark6428@users.noreply.github.com>
2023-12-03 15:43:32 -0800

    comment about cms text data

--- create_db.sql
+++ create_db.sql
@@ -4,6 +4,7 @@ use City_of_Williamston;
 
 create table UserInfo(Username varchar(20) NOT NULL, PwHash varbinary(1024) NOT NULL NOT NULL, CDate datetime, PRIMARY KEY(Username));
 
+-- holds text data for the cms community page
 create table cmsCommunityPage (
   number int auto_increment primary key,
   size varchar(10) not null,
