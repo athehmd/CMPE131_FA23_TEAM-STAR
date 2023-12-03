d7ab7a11fe8524aeaaa54fb284732d503322e6a5
Mark6428 <143025211+Mark6428@users.noreply.github.com>
2023-12-03 15:41:35 -0800

    added cms text data to database

--- create_db.sql
+++ create_db.sql
@@ -4,3 +4,13 @@ use City_of_Williamston;
 
 create table UserInfo(Username varchar(20) NOT NULL, PwHash varbinary(1024) NOT NULL NOT NULL, CDate datetime, PRIMARY KEY(Username));
 
+create table cmsCommunityPage (
+  number int auto_increment primary key,
+  size varchar(10) not null,
+  content text
+  );
+
+insert into cmsCommunityPage (size,content)
+values ('h1','Sample Title'),
+('h3','Sample Heading'),
+('p','Sample Text');
